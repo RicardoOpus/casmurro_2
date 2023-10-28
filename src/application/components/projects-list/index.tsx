eb360f17209bb18b3b65ce21ee1e98a9a232d7a2
ricardoOpus <ricardoopus125@gmail.com>
2023-10-28 05:28:21 -0300

    Refactor: Projects, send all logic of filter and Pagination project-list component

--- src/application/components/projects-list/index.tsx
+++ src/application/components/projects-list/index.tsx
@@ -1,14 +1,119 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import Project from '../../../domain/projectModel';
 import { convertDateBR, convertToTime } from '../../../service/utils';
 import './projects-list.css';
+import {
+  currentPage,
+  nextPage, previousPage, totalPages,
+} from '../../redux/actions';
+
+type RootState = {
+  paginationReducer: {
+    currentPage: number,
+    totalPages: number
+  }
+};
 
 function ProjectList({ projects }: { projects: Project[] }) {
+  const { paginationReducer } = useSelector((state: RootState) => state);
+  const projectsPerPage = 5;
+  const [selectedStatus, setSelectedStatus] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState('');
+  const [selectedTitle, setSelectedTitle] = useState('');
+  const [clearFilters, setClearFilters] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const goNextPage = () => {
+    if (paginationReducer.currentPage === paginationReducer.totalPages) {
+      return null;
+    }
+    if (paginationReducer.currentPage < Math.ceil(projects.length / 5)) {
+      dispatch(nextPage());
+    }
+    return undefined;
+  };
+
+  const goPreviousPage = () => {
+    if (paginationReducer.currentPage > 1) {
+      dispatch(previousPage());
+    }
+  };
+
+  const filteredProjects = projects.filter((project) => {
+    const statusMatch = !selectedStatus || project.status === selectedStatus;
+    const genreMatch = !selectedGenre || project.literary_genre === selectedGenre;
+    const titleMatch = !selectedTitle || project.title.includes(selectedTitle);
+    return statusMatch && genreMatch && titleMatch;
+  });
+
+  const totalPagesList = Math.ceil(filteredProjects.length / projectsPerPage);
+
+  const projectsSlice = filteredProjects.slice(
+    (paginationReducer.currentPage - 1) * projectsPerPage,
+    paginationReducer.currentPage * projectsPerPage,
+  );
+
+  const clearAllFilters = () => {
+    setSelectedStatus('');
+    setSelectedGenre('');
+    setSelectedTitle('');
+    setClearFilters(true);
+  };
+
+  useEffect(() => {
+    if (clearFilters) {
+      setClearFilters(false);
+    }
+  }, [clearFilters]);
+
+  useEffect(() => {
+    dispatch(currentPage(1));
+  }, [dispatch, selectedTitle, selectedGenre, selectedStatus]);
+
+  useEffect(() => {
+    dispatch(totalPages(totalPagesList));
+  }, [dispatch, totalPagesList]);
+
   return (
-    projects.length === 0 ? (
+    projectsSlice.length === 0 ? (
       <h3>nada encontrado</h3>
     ) : (
       <div className="projectsList">
-        {projects.map((project) => (
+        <div className="filterBar">
+          <input
+            type="text"
+            value={selectedTitle}
+            placeholder="Pesquisar por título..."
+            onInput={(e) => {
+              const target = e.target as HTMLTextAreaElement;
+              setSelectedTitle(target.value);
+            }}
+          />
+          <select
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+          >
+            <option value="">Todos os Status</option>
+            <option value="Arquivado">Arquivado</option>
+            <option value="Em Andamento">Em Andamento</option>
+            <option value="Finalizado">Finalizado</option>
+            <option value="Não iniciado">Não iniciado</option>
+            <option value="Pausado">Pausado</option>
+          </select>
+          <select
+            value={selectedGenre}
+            onChange={(e) => setSelectedGenre(e.target.value)}
+          >
+            <option value="">Todos os Gêneros</option>
+            <option value="Romance">Romance</option>
+            <option value="Conto">Conto</option>
+          </select>
+          <button type="button" onClick={clearAllFilters}>Limpar Filtros</button>
+        </div>
+
+        {projectsSlice.map((project) => (
           <button key={project.id} className="projectsItens" type="button">
             <div className="projectCard">
               <div>
@@ -35,6 +140,19 @@ function ProjectList({ projects }: { projects: Project[] }) {
             </div>
           </button>
         ))}
+        <div className="button-container">
+          <button onClick={goPreviousPage} type="button">❮ </button>
+          <span>
+            Página
+            {' '}
+            {paginationReducer.currentPage}
+            {' '}
+            de
+            {' '}
+            {paginationReducer.totalPages}
+          </span>
+          <button onClick={goNextPage} type="button"> ❯</button>
+        </div>
       </div>
     )
   );
--- src/application/pages/projects/index.tsx
+++ src/application/pages/projects/index.tsx
@@ -1,32 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import casmurroLogo from '../../../../public/casmurro-logo.svg';
 import IndexedDBrepository from '../../../infra/repository/indexedDBrepository';
 import './projects.css';
 import NoData from '../../components/no-dada';
 import Project from '../../../domain/projectModel';
 import ProjectList from '../../components/projects-list';
-import {
-  currentPage, nextPage, previousPage, totalPages,
-} from '../../redux/actions';
-
-type RootState = {
-  paginationReducer: {
-    currentPage: number,
-    totalPages: number
-  }
-};
 
 function Projects() {
   const [projects, setDados] = useState<Project[]>([]);
-  const projectsPerPage = 5;
-  const [selectedStatus, setSelectedStatus] = useState('');
-  const [selectedGenre, setSelectedGenre] = useState('');
-  const [selectedTitle, setSelectedTitle] = useState('');
-  const [clearFilters, setClearFilters] = useState(false);
-
-  const { paginationReducer } = useSelector((state: RootState) => state);
-  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,48 +18,6 @@ function Projects() {
 
     fetchData();
   }, []);
-
-  useEffect(() => {
-    if (clearFilters) {
-      setClearFilters(false);
-    }
-  }, [clearFilters]);
-
-  useEffect(() => {
-    dispatch(currentPage(1));
-  }, [dispatch, selectedTitle, selectedGenre, selectedStatus]);
-
-  const filteredProjects = projects.filter((project) => {
-    const statusMatch = !selectedStatus || project.status === selectedStatus;
-    const genreMatch = !selectedGenre || project.literary_genre === selectedGenre;
-    const titleMatch = !selectedTitle || project.title.includes(selectedTitle);
-    return statusMatch && genreMatch && titleMatch;
-  });
-
-  const totalPagesList = Math.ceil(filteredProjects.length / projectsPerPage);
-
-  useEffect(() => {
-    dispatch(totalPages(totalPagesList));
-  }, [dispatch, totalPagesList]);
-
-  const goNextPage = () => {
-    if (paginationReducer.currentPage < Math.ceil(projects.length / 5)) {
-      dispatch(nextPage());
-    }
-  };
-
-  const goPreviousPage = () => {
-    if (paginationReducer.currentPage > 1) {
-      dispatch(previousPage());
-    }
-  };
-
-  const clearAllFilters = () => {
-    setSelectedStatus('');
-    setSelectedGenre('');
-    setSelectedTitle('');
-    setClearFilters(true);
-  };
 
   return (
     <div className="innerContent">
@@ -98,61 +37,13 @@ function Projects() {
             Importar
           </button>
         </div>
-        <div className="filterBar">
-          <input
-            type="text"
-            value={selectedTitle}
-            placeholder="Pesquisar por título..."
-            onInput={(e) => {
-              const target = e.target as HTMLTextAreaElement;
-              setSelectedTitle(target.value);
-            }}
-          />
-          <select
-            value={selectedStatus}
-            onChange={(e) => setSelectedStatus(e.target.value)}
-          >
-            <option value="">Todos os Status</option>
-            <option value="Arquivado">Arquivado</option>
-            <option value="Em Andamento">Em Andamento</option>
-            <option value="Finalizado">Finalizado</option>
-            <option value="Não iniciado">Não iniciado</option>
-            <option value="Pausado">Pausado</option>
-          </select>
-          <select
-            value={selectedGenre}
-            onChange={(e) => setSelectedGenre(e.target.value)}
-          >
-            <option value="">Todos os Gêneros</option>
-            <option value="Romance">Romance</option>
-            <option value="Conto">Conto</option>
-          </select>
-          <button type="button" onClick={clearAllFilters}>Limpar Filtros</button>
-        </div>
         {projects.length === 0 ? (
           <NoData dataType="projetos" />
         ) : (
           <ProjectList
-            projects={filteredProjects
-              .slice(
-                (paginationReducer.currentPage - 1) * projectsPerPage,
-                paginationReducer.currentPage * projectsPerPage,
-              )}
+            projects={projects}
           />
         )}
-        <div className="button-container">
-          <button onClick={goPreviousPage} type="button">❮ </button>
-          <span>
-            Página
-            {' '}
-            {paginationReducer.currentPage}
-            {' '}
-            de
-            {' '}
-            {paginationReducer.totalPages}
-          </span>
-          <button onClick={goNextPage} type="button"> ❯</button>
-        </div>
       </div>
     </div>
   );
