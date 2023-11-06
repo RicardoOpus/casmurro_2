d0f6993c50770f067bd3196eec7e9a409aa272cc
ricardoOpus <ricardoopus125@gmail.com>
2023-11-06 06:03:41 -0300

    Style: modal corners

--- src/application/components/generic-modal/index.tsx
+++ src/application/components/generic-modal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface GenericModalProps {
   onClose: () => void;
@@ -9,6 +9,7 @@ interface GenericModalProps {
 
 function GenericModal({ onClose, typeName, onDataSend }: GenericModalProps) {
   const [newItemTitle, SetNewItemTitle] = useState('');
+  const [uiMode, SetUiMode] = useState('light-mode');
   const isSaveButtonDisabled = newItemTitle.trim() === '';
   const handleCancel = () => onClose();
 
@@ -17,25 +18,40 @@ function GenericModal({ onClose, typeName, onDataSend }: GenericModalProps) {
     onClose();
   };
 
+  useEffect(() => {
+    const storedMode = localStorage.getItem('uiMode');
+    if (storedMode === 'light') {
+      SetUiMode('light-mode');
+    } else {
+      SetUiMode('dark-mode');
+    }
+  }, []);
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <h2>
-          Novo
-          {' '}
-          {typeName}
-        </h2>
-        <input
-          className="cardInputTitle"
-          type="text"
-          placeholder="Título"
-          value={newItemTitle}
-          onChange={(e) => SetNewItemTitle(e.target.value)}
-        />
-        <div className="button-container">
-          <button id="btnSavePr" onClick={sendDataToParent} type="button" disabled={isSaveButtonDisabled}>Salvar</button>
-          <button onClick={handleCancel} type="button">Cancelar</button>
+        <img className="ponto1" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <img className="ponto2" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <div className="modal-border">
+          <h2>
+            Novo
+            {' '}
+            {typeName}
+          </h2>
+          <input
+            className="cardInputTitle"
+            type="text"
+            placeholder="Título"
+            value={newItemTitle}
+            onChange={(e) => SetNewItemTitle(e.target.value)}
+          />
+          <div className="button-container">
+            <button id="btnSavePr" onClick={sendDataToParent} type="button" disabled={isSaveButtonDisabled}>Salvar</button>
+            <button onClick={handleCancel} type="button">Cancelar</button>
+          </div>
         </div>
+        <img className="ponto3" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <img className="ponto4" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
       </div>
     </div>
   );
--- src/application/pages/projects/new-project-modal/index.tsx
+++ src/application/pages/projects/new-project-modal/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import './newProjectModal.css';
 import 'balloon-css';
 import { useNavigate } from 'react-router-dom';
@@ -16,6 +16,7 @@ function NewProjectModal({ onClose }: NewProjectModalProps) {
   const [projectName, setProjectName] = useState('');
   const [agreedToTerms, setAgreedToTerms] = useState(false);
   const [warningTerms, setWarningTerms] = useState(false);
+  const [uiMode, SetUiMode] = useState('light-mode');
   const isSaveButtonDisabled = projectName.trim() === '';
   const handleCancel = () => onClose();
 
@@ -34,73 +35,88 @@ function NewProjectModal({ onClose }: NewProjectModalProps) {
     setAgreedToTerms(e.target.checked);
   };
 
+  useEffect(() => {
+    const storedMode = localStorage.getItem('uiMode');
+    if (storedMode === 'light') {
+      SetUiMode('light-mode');
+    } else {
+      SetUiMode('dark-mode');
+    }
+  }, []);
+
   return (
     <div className="modal" data-testid="modal-new-project">
       <div className="modal-content">
-        <h2>Novo Projeto</h2>
-        <input
-          className="cardInputTitle"
-          type="text"
-          placeholder="Título do projeto"
-          value={projectName}
-          onChange={(e) => setProjectName(e.target.value)}
-        />
-        <label htmlFor="termos-uso">
+        <img className="ponto1" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <img className="ponto2" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <div className="modal-border">
+          <h2>Novo Projeto</h2>
           <input
-            id="termos-uso"
-            type="checkbox"
-            onChange={handleAgreeToTerms}
+            className="cardInputTitle"
+            type="text"
+            placeholder="Título do projeto"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
           />
-          {' '}
-          Li e concordo com os termos de uso.
-        </label>
-        {(warningTerms) && (
-          <p className="tooltip-big-text" data-balloon-visible aria-label="Você deve concordar com os termos." data-balloon-pos="down" />
-        )}
-        <div className="termos-uso">
-          <p>
-            Ao marcar esta caixa, você está consentindo com os seguintes termos
+          <label htmlFor="termos-uso">
+            <input
+              id="termos-uso"
+              type="checkbox"
+              onChange={handleAgreeToTerms}
+            />
             {' '}
-            e condições relacionados ao uso deste programa:
-          </p>
-          <p>
-            1. O programa é gratuito e sem garantias: Este programa é disponibilizado
-            {' '}
-            gratuitamente e não oferece garantias de qualquer tipo. Você entende que o programa
-            {' '}
-            é fornecido &quot;no estado em que se encontra&quot; e que o uso é por sua própria
-            {' '}
-            contae risco. Não há garantias de desempenho,
-            {' '}
-            confiabilidade ou adequação a qualquer finalidade específica.
-          </p>
-          <p>
-            2. Responsabilidade pelo backup de arquivos:
-            {' '}
-            É de sua responsabilidade fazer regularmente cópias de
-            {' '}
-            segurança de seus arquivos e dados. O programa pode estar sujeito a falhas e
-            {' '}
-            problemas técnicos, e não nos responsabilizamos por quaisquer
-            {' '}
-            perdas ou danos aos seus arquivos.
-          </p>
-          <p>
-            3. Testes exaustivos, mas sem garantia de ausência de erros: Embora
-            {' '}
-            tenhamos feito testes exaustivos, você compreende que nenhum programa
-            {' '}
-            de software está livre de erros. Não podemos garantir que o programa estará
-            {' '}
-            isento de erros ou que atenderá a todas as suas expectativas.
-            {' '}
-            Você reconhece que podem ocorrer problemas imprevistos durante o uso.
-          </p>
+            Li e concordo com os termos de uso.
+          </label>
+          {(warningTerms) && (
+            <p className="tooltip-big-text" data-balloon-visible aria-label="Você deve concordar com os termos." data-balloon-pos="down" />
+          )}
+          <div className="termos-uso">
+            <p>
+              Ao marcar esta caixa, você está consentindo com os seguintes termos
+              {' '}
+              e condições relacionados ao uso deste programa:
+            </p>
+            <p>
+              1. O programa é gratuito e sem garantias: Este programa é disponibilizado
+              {' '}
+              gratuitamente e não oferece garantias de qualquer tipo. Você entende que o programa
+              {' '}
+              é fornecido &quot;no estado em que se encontra&quot; e que o uso é por sua própria
+              {' '}
+              contae risco. Não há garantias de desempenho,
+              {' '}
+              confiabilidade ou adequação a qualquer finalidade específica.
+            </p>
+            <p>
+              2. Responsabilidade pelo backup de arquivos:
+              {' '}
+              É de sua responsabilidade fazer regularmente cópias de
+              {' '}
+              segurança de seus arquivos e dados. O programa pode estar sujeito a falhas e
+              {' '}
+              problemas técnicos, e não nos responsabilizamos por quaisquer
+              {' '}
+              perdas ou danos aos seus arquivos.
+            </p>
+            <p>
+              3. Testes exaustivos, mas sem garantia de ausência de erros: Embora
+              {' '}
+              tenhamos feito testes exaustivos, você compreende que nenhum programa
+              {' '}
+              de software está livre de erros. Não podemos garantir que o programa estará
+              {' '}
+              isento de erros ou que atenderá a todas as suas expectativas.
+              {' '}
+              Você reconhece que podem ocorrer problemas imprevistos durante o uso.
+            </p>
+          </div>
+          <div className="button-container">
+            <button id="btnSavePr" onClick={handleCreate} type="button" disabled={isSaveButtonDisabled}>Salvar</button>
+            <button onClick={handleCancel} type="button">Cancelar</button>
+          </div>
         </div>
-        <div className="button-container">
-          <button id="btnSavePr" onClick={handleCreate} type="button" disabled={isSaveButtonDisabled}>Salvar</button>
-          <button onClick={handleCancel} type="button">Cancelar</button>
-        </div>
+        <img className="ponto3" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
+        <img className="ponto4" src={uiMode === 'light-mode' ? './corner.png' : 'corner-dark.png'} alt="ponto" />
       </div>
     </div>
   );
