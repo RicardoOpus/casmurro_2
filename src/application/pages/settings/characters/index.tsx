22bd7947ec661ac622422a7f9e8eb08a53c5ba3b
ricardoOpus <ricardoopus125@gmail.com>
2023-11-23 15:19:50 -0300

    Feat: creat a component for characters settings

---
+++ src/application/pages/settings/characters/index.tsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import EditLists from '../edit-lists';
+import IrootStateProject from '../../../../domain/IrootStateProject';
+import indexedDBrepository from '../../../../infra/repository/indexedDBrepository';
+import { fetchProjectDataAction } from '../../../redux/actions/projectActions';
+
+function CharactersSettings() {
+  const dispatch = useDispatch();
+  const prjSettings = useSelector((state: IrootStateProject) => (
+    state.projectDataReducer.projectData.projectSettings));
+
+  const saveSettings = async (newSettings: string[], table: string) => {
+    await indexedDBrepository.updateProjectSettingsList(newSettings, table);
+    dispatch(fetchProjectDataAction(true));
+  };
+
+  return (
+    <div>
+      <h1>CharactersSettings</h1>
+      <fieldset>
+        <legend>
+          <img className="settingsSectionIcon icon-color" src="./characters.png" alt="Character icon" />
+          {' '}
+          Personagens
+        </legend>
+        <div className="listsSection">
+          <EditLists list={prjSettings.charactersCategory} projSettingsI="charactersCategory" listTitle="Categorias" onDataSend={saveSettings} />
+          <EditLists list={prjSettings.charactersGenders} projSettingsI="charactersGenders" listTitle="Gênero" onDataSend={saveSettings} />
+          <EditLists list={prjSettings.charactersCoreGroupes} projSettingsI="charactersCoreGroupes" listTitle="Núcleos" onDataSend={saveSettings} />
+        </div>
+      </fieldset>
+    </div>
+  );
+}
+
+export default CharactersSettings;
--- src/application/pages/settings/index.tsx
+++ src/application/pages/settings/index.tsx
@@ -1,21 +1,8 @@
-import { useDispatch, useSelector } from 'react-redux';
-import IrootStateProject from '../../../domain/IrootStateProject';
 import './settings.css';
-import EditLists from './edit-lists';
-import indexedDBrepository from '../../../infra/repository/indexedDBrepository';
-import { fetchProjectDataAction } from '../../redux/actions/projectActions';
+import CharactersSettings from './characters';
 import GeneralSettings from './general';
 
 function Settings() {
-  const dispatch = useDispatch();
-  const prjSettings = useSelector((state: IrootStateProject) => (
-    state.projectDataReducer.projectData.projectSettings));
-
-  const saveSettings = async (newSettings: string[], table: string) => {
-    await indexedDBrepository.updateProjectSettingsList(newSettings, table);
-    dispatch(fetchProjectDataAction(true));
-  };
-
   return (
     <div className="innerContent">
       <div className="card">
@@ -25,18 +12,7 @@ function Settings() {
           Qualquer alteração feita aqui não afeta outros projetos.
         </p>
         <GeneralSettings />
-        <fieldset>
-          <legend>
-            <img className="settingsSectionIcon icon-color" src="./characters.png" alt="Character icon" />
-            {' '}
-            Personagens
-          </legend>
-          <div className="listsSection">
-            <EditLists list={prjSettings.charactersCategory} projSettingsI="charactersCategory" listTitle="Categorias" onDataSend={saveSettings} />
-            <EditLists list={prjSettings.charactersGenders} projSettingsI="charactersGenders" listTitle="Gênero" onDataSend={saveSettings} />
-            <EditLists list={prjSettings.charactersCoreGroupes} projSettingsI="charactersCoreGroupes" listTitle="Núcleos" onDataSend={saveSettings} />
-          </div>
-        </fieldset>
+        <CharactersSettings />
       </div>
     </div>
   );
