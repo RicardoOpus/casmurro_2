0bfb377568364c5b55d95ed2364b52b3ad85ed56
ricardoOpus <ricardoopus125@gmail.com>
2023-11-25 20:55:15 -0300

    Feat: sidebar inspect world

--- src/application/components/side-bar/card-inspect/world-inspect/index.tsx
+++ src/application/components/side-bar/card-inspect/world-inspect/index.tsx
@@ -1,10 +1,33 @@
-import ICardInspectProps from '../../../../../domain/IcardInspectProps';
+import { useNavigate } from 'react-router-dom';
+import IWorld from '../../../../../domain/worldModel';
 
-function WorldInspect({ card }: ICardInspectProps) {
+interface CardInspectProps {
+  card: IWorld;
+}
+
+function WorldInspect({ card }: CardInspectProps) {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>WorldInspect</h1>
-      <h1>{card.title}</h1>
+    <div className="inspectCard">
+      <div className="imageInspect">
+        {card.image && (
+          <img className="imageInscpet" src={card.image} id="output" alt="character" />
+        )}
+      </div>
+      <div className="inspectTitle">
+        <button onClick={() => navigate(`/world/${card.id}`)} className="btnInvisible" type="button">{card.title}</button>
+      </div>
+      <div className="inspecInfos">
+        {card.category ? <span>Categoria:</span> : ''}
+        <p>{card.category}</p>
+      </div>
+      {card.resume ? <span>Resumo:</span> : ''}
+      <p className="PtextInfos">{card.resume}</p>
+      {card.note ? <span>Notas:</span> : ''}
+      <p className="PtextInfos">{card.note}</p>
+      {card.content ? <span>Conteúdo:</span> : ''}
+      <p className="PtextInfos">{card.content}</p>
     </div>
   );
 }
