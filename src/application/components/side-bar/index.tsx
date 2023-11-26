d9afc7b1a0e907c7e43fc05b720e126fbee46d6c
ricardoOpus <ricardoopus125@gmail.com>
2023-11-26 01:14:56 -0300

    Feat: implements sidebar resizible

--- src/application/components/side-bar/index.tsx
+++ src/application/components/side-bar/index.tsx
@@ -1,12 +1,16 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import {
+  ChangeEvent, SyntheticEvent, useEffect, useState,
+} from 'react';
 import './side-bar.css';
 import { useSelector } from 'react-redux';
+import { Resizable, ResizeCallbackData } from 'react-resizable';
 import IrootStateProject from '../../../domain/IrootStateProject';
 import IWorld from '../../../domain/worldModel';
 import ICharacter from '../../../domain/characterModel';
 import CardInspect from './card-inspect';
 
 function SideBar() {
+  const [width, setWidth] = useState(400);
   const { projectData } = useSelector((state: IrootStateProject) => state.projectDataReducer);
   const [isSidebarOpen, setSidebarOpen] = useState(true);
   const [searchInput, setSearchInput] = useState('');
@@ -15,11 +19,24 @@ function SideBar() {
   const [filtredCards, setFiltredCards] = useState<(IWorld | ICharacter)[]>([]);
   const [selectedCard, setSelectedCar] = useState<IWorld | ICharacter>();
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
+  const onResize = (
+    _e: SyntheticEvent<Element, Event>,
+    data: ResizeCallbackData,
+  ) => {
+    if (data.size && Number(data.size.width) > 60) {
+      setWidth(data.size.width);
+    }
   };
 
-  const sidebarWidth = isSidebarOpen ? 'block' : 'none';
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+    setWidth(60);
+  };
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+    setWidth(400);
+  };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
@@ -53,59 +70,66 @@ function SideBar() {
   }, [allCards, searchInput]);
 
   return (
-    <div className="sideBar">
-      <button className="btnDiscret sideBarButton" type="button" onClick={toggleSidebar}>
-        {isSidebarOpen ? '❮❮' : '❯❯'}
-      </button>
-
-      <div className="sideBarContent" style={{ display: sidebarWidth }}>
-        {isSidebarOpen && (
-          <div>
-            <h3>Consulta rápida</h3>
-            <input
-              type="text"
-              value={searchInput}
-              placeholder="Pesquisar em tudo..."
-              onChange={(e) => handleInputChange(e)}
-              className="cardInputSearch"
-            />
-            <div className="inspectBottons">
-              <button onClick={() => setSearchInput('')} className="btnSmall" type="button">Limpar</button>
-              <button onClick={() => setShowInspect(false)} disabled={!showInspect} className="btnSmall" type="button">
-                <span className="ui-icon ui-icon-arrowreturnthick-1-w icon-color" />
-                Voltar
-              </button>
-            </div>
-            {showInspect ? (
-              <div>
-                {selectedCard && (
-                  <CardInspect card={selectedCard} />
-                )}
-              </div>
-            ) : (
-              filtredCards.map((e) => (
-                <div key={e.id}>
-                  <button
-                    className="btnInvisible"
-                    type="button"
-                    onClick={() => sideBarHandleClick(e)}
-                  >
-                    {e.title}
-                    <br />
-                    {' '}
-                    <span className="spanSideBar">
-                      (
-                      {e.type}
-                      )
-                    </span>
-                  </button>
-                </div>
-              ))
-            )}
-          </div>
+    <Resizable className="resizable" width={width} height={100} onResize={onResize} handle={<div className="custom-handle" />}>
+      <div className="sideBar" style={{ width: `${width}px` }}>
+        {isSidebarOpen ? (
+          <button className="btnDiscret sideBarButton" type="button" onClick={closeSidebar}>
+            ❮❮
+          </button>
+        ) : (
+          <button className="btnDiscret sideBarButton" type="button" onClick={openSidebar}>
+            ❯❯
+          </button>
         )}
+        <div className="sideBarContent">
+          {isSidebarOpen && (
+            <div>
+              <h3>Consulta rápida</h3>
+              <input
+                type="text"
+                value={searchInput}
+                placeholder="Pesquisar em tudo..."
+                onChange={(e) => handleInputChange(e)}
+                className="cardInputSearch"
+              />
+              <div className="inspectBottons">
+                <button onClick={() => setShowInspect(false)} disabled={!showInspect} className="btnSmall" type="button">
+                  <span className="ui-icon ui-icon-arrowreturnthick-1-w icon-color" />
+                  Voltar
+                </button>
+                <button onClick={() => setSearchInput('')} className="btnSmall" type="button">Limpar</button>
+              </div>
+              {showInspect ? (
+                <div>
+                  {selectedCard && (
+                    <CardInspect card={selectedCard} />
+                  )}
+                </div>
+              ) : (
+                filtredCards.map((e) => (
+                  <div key={e.id}>
+                    <button
+                      className="btnInvisible"
+                      type="button"
+                      onClick={() => sideBarHandleClick(e)}
+                    >
+                      {e.title}
+                      <br />
+                      {' '}
+                      <span className="spanSideBar">
+                        (
+                        {e.type}
+                        )
+                      </span>
+                    </button>
+                  </div>
+                ))
+              )}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Resizable>
   );
 }
 
