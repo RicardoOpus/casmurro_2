d173f6417d73208813af7b4ab63739599c82b13c
ricardoOpus <ricardoopus125@gmail.com>
2023-11-29 05:39:52 -0300

    Feat: update Inspect World

--- src/application/components/side-bar/card-inspect/world-inspect/index.tsx
+++ src/application/components/side-bar/card-inspect/world-inspect/index.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import IWorld from '../../../../../domain/worldModel';
+import utils from '../../../../../service/utils';
 
 interface CardInspectProps {
   card: IWorld;
@@ -21,6 +23,24 @@ function WorldInspect({ card }: CardInspectProps) {
       <div className="inspecInfos">
         {card.category ? <span>Categoria:</span> : ''}
         <p>{card.category}</p>
+        {card.date ? <span>Data:</span> : ''}
+        <p>{utils.convertDatePTBR(card.date)}</p>
+      </div>
+      <div className="inspectCharRelations">
+        {card.link_list && card.link_list.length > 0 ? <span>Links:</span> : ''}
+        {card.link_list?.map((e) => (
+          <div key={uuidv4()}>
+            <a href={e.URL} target="_blank" rel="noreferrer">{e.linkName}</a>
+          </div>
+        ))}
+      </div>
+      <div className="inspectCharRelations">
+        {card.task_list && card.task_list.length > 0 ? <span>Tarefas:</span> : ''}
+        {card.task_list?.map((e) => (
+          <div key={uuidv4()}>
+            <p className={e.isDone ? 'doneTask spanTaskLit' : 'spanTaskLit'}>{`• ${e.task}`}</p>
+          </div>
+        ))}
       </div>
       {card.resume ? <span>Resumo:</span> : ''}
       <p className="PtextInfos">{card.resume}</p>
