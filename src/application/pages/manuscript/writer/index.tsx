d580f530d83e17b6dfc1f043ae1cc1f704dfa933
ricardoOpus <ricardoopus125@gmail.com>
2023-12-06 09:33:49 -0300

    Feat: implements color schemes in Manuscript

--- src/application/pages/manuscript/writer/index.tsx
+++ src/application/pages/manuscript/writer/index.tsx
@@ -12,10 +12,14 @@ function Writer() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const [colapseState, setColapseState] = useState(false);
-  const typeSet = localStorage.getItem('sceneTypeFont') || 'Texgyretermes';
-  const [typeFontUser, setTypeFontUSer] = useState(typeSet);
-  const fontSizeSet = localStorage.getItem('sceneSize') || '25px';
-  const [sizeFontUser, setSizeFontUser] = useState(fontSizeSet);
+  const storagetypeFont = localStorage.getItem('sceneTypeFont') || 'Texgyretermes';
+  const [stateFontUser, setStateFontUSer] = useState(storagetypeFont);
+  const storageFontSizeSet = localStorage.getItem('sceneSize') || '25px';
+  const [stateSizeFontUser, setStateSizeFontUser] = useState(storageFontSizeSet);
+  const storageColorScheme = localStorage.getItem('sceneColorScheme') || 'darkScene';
+  const [stateColorScheme, setStateColorScheme] = useState(storageColorScheme);
+  const storageColorHex = localStorage.getItem('sceneColorHex') || '#878787';
+  const [stateColorHex, setStateColorHex] = useState(storageColorHex);
   const manuscriptItens = useSelector((state: IrootStateProject) => (
     state.projectDataReducer.projectData.data?.manuscript));
   const currentMItem = manuscriptItens?.find((e) => e.id === Number(id));
@@ -33,7 +37,7 @@ function Writer() {
 
   const adjustTextSize = (increaseStep: number, increase: boolean) => {
     const root = document.documentElement;
-    const currentSizeInPixels = parseInt(fontSizeSet, 10);
+    const currentSizeInPixels = parseInt(storageFontSizeSet, 10);
     let newSizeInPixels;
     if (increase) {
       newSizeInPixels = currentSizeInPixels + increaseStep;
@@ -42,14 +46,79 @@ function Writer() {
     }
     root.style.setProperty('--user-scene-size', `${newSizeInPixels}px`);
     localStorage.setItem('sceneSize', `${newSizeInPixels}px`);
-    setSizeFontUser(`${newSizeInPixels}px`);
+    setStateSizeFontUser(`${newSizeInPixels}px`);
   };
 
   const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const root = document.documentElement;
     root.style.setProperty('--user-scene-type', e.target.value);
     localStorage.setItem('sceneTypeFont', e.target.value);
-    setTypeFontUSer(e.target.value);
+    setStateFontUSer(e.target.value);
+  };
+
+  const handleColoScheme = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === 'lightScene') {
+      localStorage.setItem('sceneColorScheme', 'lightScene');
+      localStorage.setItem('sceneColorHex', '#000000');
+      setStateColorHex('#000000');
+      setStateColorScheme(e.target.value);
+    } if (e.target.value === 'sepiaScene') {
+      localStorage.setItem('sceneColorScheme', 'sepiaScene');
+      localStorage.setItem('sceneColorHex', '#5d4232');
+      setStateColorHex('#5d4232');
+      setStateColorScheme(e.target.value);
+    } if (e.target.value === 'darkScene') {
+      localStorage.setItem('sceneColorScheme', 'darkScene');
+      localStorage.setItem('sceneColorHex', '#878787');
+      setStateColorHex('#878787');
+      setStateColorScheme(e.target.value);
+    }
+  };
+
+  const handleDecoration = (type: string) => {
+    switch (type) {
+      case 'Texgyretermes':
+        return (
+          <span className="ornament1" style={{ backgroundColor: stateColorHex }} />
+        );
+      case 'Roboto':
+        return (
+          <span style={{ marginRight: '.2em' }}>●</span>
+        );
+      case 'TypeCurier':
+        return (
+          <span style={{ marginRight: '.2em' }}>--</span>
+        );
+      default:
+        return (
+          <div>
+            <span />
+          </div>
+        );
+    }
+  };
+
+  const handleDecoration2 = (type: string) => {
+    switch (type) {
+      case 'Texgyretermes':
+        return (
+          <span className="ornament2" style={{ backgroundColor: stateColorHex }} />
+        );
+      case 'Roboto':
+        return (
+          <span style={{ marginLeft: '.2em' }}>●</span>
+        );
+      case 'TypeCurier':
+        return (
+          <span style={{ marginLeft: '.2em' }}>--</span>
+        );
+      default:
+        return (
+          <div>
+            <span />
+          </div>
+        );
+    }
   };
 
   const colapeDetails = (colapse: boolean) => {
@@ -74,12 +143,12 @@ function Writer() {
         ) : (
           <button onClick={() => colapeDetails(false)} className="btnWriter" type="button">🡻</button>
         )}
-        <button onClick={() => adjustTextSize(1, true)} className="btnSmall" type="button">+ A</button>
-        <button onClick={() => adjustTextSize(1, false)} className="btnSmall" type="button">- A</button>
+        <button onClick={() => adjustTextSize(1, true)} className="btnWriter" type="button">+ A</button>
+        <button onClick={() => adjustTextSize(1, false)} className="btnWriter" type="button">- A</button>
         <select
-          className="ui-button ui-corner-all"
+          className="ui-button ui-corner-all writeSelect"
           onChange={(e) => handleSelectChange(e)}
-          value={typeFontUser}
+          value={stateFontUser}
           style={{ color: 'var(--text-color-inactive)' }}
         >
           <option disabled>Tido da Fonte</option>
@@ -87,17 +156,28 @@ function Writer() {
           <option value="Roboto"> • Sem Serifa</option>
           <option value="TypeCurier"> • Mono</option>
         </select>
+        <select
+          className="ui-button ui-corner-all writeSelect"
+          onChange={(e) => handleColoScheme(e)}
+          value={stateColorScheme}
+          style={{ color: 'var(--text-color-inactive)' }}
+        >
+          <option disabled>Esquema de cores</option>
+          <option value="lightScene"> • Claro</option>
+          <option value="sepiaScene"> • Sépia</option>
+          <option value="darkScene"> • Escuro</option>
+        </select>
       </div>
-      <div className="writerContainter">
-        <h1 className="writerTitle" style={{ fontFamily: typeFontUser }}>
-          <span className="ornament1" />
+      <div className={`writerContainter ${stateColorScheme}`}>
+        <h1 className="writerTitle" style={{ fontFamily: stateFontUser }}>
+          {handleDecoration(stateFontUser)}
           {stateMItem.title}
-          <span className="ornament2" />
+          {handleDecoration2(stateFontUser)}
         </h1>
         <div>
           <textarea
             className="writeArea"
-            style={{ fontFamily: typeFontUser, fontSize: sizeFontUser }}
+            style={{ fontFamily: stateFontUser, fontSize: stateSizeFontUser }}
             value={stateMItem?.content}
             onChange={(e) => handleTextAreaChange(e, 'content')}
             placeholder="Não iniciado..."
