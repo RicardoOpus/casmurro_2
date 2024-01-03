92c51aa5bf8546939aa3d2893675ed5fbc924f66
ricardoOpus <ricardoopus125@gmail.com>
2024-01-03 05:50:59 -0300

    feat: implements filtes in timeline

--- src/application/pages/timeline/index.tsx
+++ src/application/pages/timeline/index.tsx
@@ -15,7 +15,78 @@ interface ValidDateItem extends Required<IManuscript>, Required<IWorld>, Require
 
 function Timeline() {
   const { projectData } = useSelector((state: IrootStateProject) => state.projectDataReducer);
-  const [allCards, setAllCards] = useState<(IWorld | ICharacter | IManuscript)[]>([]);
+  const charList = useSelector((state: IrootStateProject) => (
+    state.projectDataReducer.projectData.data?.characters));
+  const [allCards, setAllCards] = useState<(ValidDateItem)[]>([]);
+  const [filtredTimeline, setFiltredTimeline] = useState<(IWorld | ICharacter | IManuscript)[]>([]);
+  const [selectedChar, setSelectedChar] = useState(0);
+  const [isAscOrder, setIsAscOrdere] = useState(true);
+
+  const renderItem = (item: ValidDateItem) => {
+    switch (item.type) {
+      case 'Personagem':
+        return (
+          <div className="timeline-section">
+            <p style={{ background: `linear-gradient(to right, ${item.color} 0%, var(--bg-color) 25%)`, color: 'black' }}>{utils.convertDatePTBR(item.date)}</p>
+            <p>{item.date_birth ? `Nasce ${item.title}` : `Morre ${item.title}`}</p>
+          </div>
+        );
+      default:
+        return (
+          <div className="timeline-section">
+            <p style={{ backgroundColor: 'var(--bg-color)' }}>{utils.convertDatePTBR(item.date)}</p>
+            <p>{item.title}</p>
+            <p style={{ color: 'var(--text-color-inactive)' }}>{item.resume}</p>
+          </div>
+        );
+    }
+  };
+
+  const renderSelectPOV = () => (
+    <div>
+      <select
+        value={selectedChar}
+        onChange={(e) => setSelectedChar(Number(e.target.value))}
+        style={{ color: 'var(--text-color-inactive)' }}
+      >
+        <option value="">Personagem</option>
+        {charList?.map((char) => (
+          <option key={char.id} value={char.id}>
+            •
+            {' '}
+            {char.title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const clearAllFilters = () => {
+    setSelectedChar(0);
+  };
+
+  const handleSort = () => {
+    const sortedList = [...filtredTimeline].reverse();
+    setFiltredTimeline(sortedList);
+    setIsAscOrdere(!isAscOrder);
+  };
+
+  useEffect(() => {
+    const handleFilter = () => {
+      const result = allCards.filter((item) => {
+        const CharacterMatch = !selectedChar || item.id === selectedChar
+          || item.pov_id === selectedChar;
+        return CharacterMatch;
+      });
+      if (!isAscOrder) {
+        const sortedList = [...result].reverse();
+        setFiltredTimeline(sortedList);
+      } else {
+        setFiltredTimeline(result);
+      }
+    };
+    handleFilter();
+  }, [allCards, isAscOrder, selectedChar]);
 
   useEffect(() => {
     const array1 = projectData.data?.manuscript?.filter((e) => e.date);
@@ -38,39 +109,25 @@ function Timeline() {
           const dateB = new Date((b as ValidDateItem).date);
           return dateA.getTime() - dateB.getTime();
         });
-        setAllCards(newArray);
+        setAllCards(newArray as ValidDateItem[]);
       }
     }
   }, [projectData.data?.characters, projectData.data?.manuscript, projectData.data?.world]);
-
-  const renderItem = (item: ValidDateItem) => {
-    switch (item.type) {
-      case 'Personagem':
-        return (
-          <div className="timeline-section">
-            <p style={{ background: `linear-gradient(to right, ${item.color} 0%, var(--bg-color) 25%)`, color: 'black' }}>{utils.convertDatePTBR(item.date)}</p>
-            <p>{item.date_birth ? `Nasce ${item.title}` : `Morre ${item.title}`}</p>
-          </div>
-        );
-      default:
-        return (
-          <div className="timeline-section">
-            <p style={{ backgroundColor: 'var(--bg-color)' }}>{utils.convertDatePTBR(item.date)}</p>
-            <p>{item.title}</p>
-            <p style={{ color: 'var(--text-color-inactive)' }}>{item.resume}</p>
-          </div>
-        );
-    }
-  };
 
   return (
     <div className="innerContent">
       <div className="card">
         <h1>Timeline</h1>
+        <div className="filterBar">
+          {renderSelectPOV()}
+          <button className="btnSmall" type="button" onClick={handleSort} disabled={isAscOrder}>↑</button>
+          <button className="btnSmall" type="button" onClick={handleSort} disabled={!isAscOrder}>↓</button>
+          <button className="btnSmall" type="button" onClick={clearAllFilters}>✖ Filtros</button>
+        </div>
         <div className="containerTimeline">
           <div className="wrapper">
             <ul className="sessions">
-              {allCards.map((e) => (
+              {filtredTimeline.map((e) => (
                 <li key={uuidv4()}>
                   {renderItem(e as ValidDateItem)}
                 </li>
