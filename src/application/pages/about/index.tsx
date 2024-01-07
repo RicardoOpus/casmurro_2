50a531c367c2cb6204c600cbdc78ea8bd8fcc59a
ricardoOpus <ricardoopus125@gmail.com>
2024-01-07 04:33:31 -0300

    chore: about page text

--- src/application/pages/about/index.tsx
+++ src/application/pages/about/index.tsx
@@ -6,10 +6,15 @@ function About() {
       <div className="card">
         <h1>Informações</h1>
         <h2>{text.about.title}</h2>
-        <div className="longtext" dangerouslySetInnerHTML={{ __html: text.about.text }} />
+        <div style={{ display: 'flex' }}>
+          <img className="projectImage" src="images/ilustra-casmurro.jpg" alt="project illutration" />
+          <div className="longtext" dangerouslySetInnerHTML={{ __html: text.about.text }} />
+        </div>
         <h2>{text.about.title2}</h2>
-        <div className="longtext" dangerouslySetInnerHTML={{ __html: text.about.text2 }} />
-        <a href="https://www.ricardoserafim.com.br" target="_blank" rel="noreferrer">https://www.ricardoserafim.com.br</a>
+        <div>
+          <div className="longtext" dangerouslySetInnerHTML={{ __html: text.about.text2 }} />
+          <a href="https://www.ricardoserafim.com.br" target="_blank" rel="noreferrer">https://www.ricardoserafim.com.br</a>
+        </div>
       </div>
     </div>
   );
